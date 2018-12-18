b5217436a5b80a77e11e640597ed3994a0137b9a
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-17 22:21:10 -0800

    Generate gifs using a nicer palette
    Also first time I actually understood ffmpeg lol. Thank you this article! https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/

--- script/gif-gen.sh
+++ script/gif-gen.sh
@@ -2,8 +2,11 @@
 set -ex
 
 frame_pattern="/tmp/gif/frame%004d.png"
+palette="/tmp/palette.png"
 
 node build/gif-gen.bundle.js
 
-# Node bunch them together into a gif! (?)
-ffmpeg -f image2 -framerate 30 -i "$frame_pattern" build/out.gif
+# Then bunch them together into a gif! (?)
+# Thanks Giphy Engineering!! https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+ffmpeg -f image2 -i "$frame_pattern" -filter_complex "[0:v] palettegen" -y $palette
+ffmpeg -f image2 -i "$frame_pattern" -framerate 30 -i $palette -filter_complex "[0:v][1:v] paletteuse" -y build/out.gif
