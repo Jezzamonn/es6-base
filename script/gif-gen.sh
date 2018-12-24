0973c581deeca73f0dd2cdb644d5a8eedffb13fb
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-24 15:39:17 -0800

    Clear things when regenerating a gif...

--- script/gif-gen.sh
+++ script/gif-gen.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+# Generates a gif using ffmpeg
+#
+# Always overwrites any existing gif.
+
 set -ex
 
-frame_pattern="/tmp/gif/frame%004d.png"
-palette="/tmp/palette.png"
+frame_dir="/tmp/gif/"
+frame_pattern="${frame_dir}frame%004d.png"
+palette="/tmp/gif/palette.png"
+
+# First clear the temp files
+rm -r "$frame_dir"
+rm "$palette"
 
 # output the frames
 node build/gif-gen.bundle.js
 
 # Then bunch them together into a gif! (?)
 # Thanks Giphy Engineering!! https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
-ffmpeg -f image2 -i "$frame_pattern" -filter_complex "[0:v] palettegen" -y $palette
+ffmpeg -f image2 -i "$frame_pattern" -filter_complex "[0:v] palettegen" -y "$palette"
 ffmpeg -f image2 -i "$frame_pattern" -framerate 30 -i $palette -filter_complex "[0:v][1:v] paletteuse" -y build/out.gif
