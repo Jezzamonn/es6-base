31eaba7e0f9de0a152c02ec652f49458a7d27733
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-02-13 19:00:48 -0800

    Yeah just optimise the gif with gifsicle

--- script/gif-gen.sh
+++ script/gif-gen.sh
@@ -23,4 +23,5 @@ node build/save-frames.bundle.js --width=500 --height=500 --out="$temp_dir"
 ffmpeg -f image2 -i "$frame_pattern" -filter_complex "[0:v] palettegen" -y "$palette"
 ffmpeg -f image2 -i "$frame_pattern" -framerate 30 -i $palette -filter_complex "[0:v][1:v] paletteuse" -y "${out_filename}"
 
-# TODO? Optimise with gifsicle? E.g. `gifsicle --optimize --color=16 "${out_filename}" -o "${out_filename}"
+# Optimise with gifsicle
+gifsicle --optimize --color=16 "${out_filename}" -o "${out_filename}"
