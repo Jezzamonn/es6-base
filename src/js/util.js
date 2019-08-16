2c01aa17752d2ae26ba41de78f7e3ac49e5df7a7
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-08-16 07:49:00 -0700

    Add a simple seeded random number generator

--- src/js/util.js
+++ src/js/util.js
@@ -74,3 +74,36 @@ export function gray(whiteAmt) {
 	const whiteRgb = Math.floor(255 * whiteAmt);
 	return rgb(whiteRgb, whiteRgb, whiteRgb);
 }
+
+// Pseudo-random number generator functions
+// From stack overflow: https://stackoverflow.com/a/47593316
+function xmur3(str) {
+    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
+        h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
+        h = h << 13 | h >>> 19;
+    return function() {
+        h = Math.imul(h ^ h >>> 16, 2246822507);
+        h = Math.imul(h ^ h >>> 13, 3266489909);
+        return (h ^= h >>> 16) >>> 0;
+    }
+}
+
+// Just using a simple 32-bit random number generator, our numbers don't need to be too random.
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
+/**
+ * 
+ * @param {string} seed What ya seed it with.
+ * @returns {function():number} A wonderful seeded random number generator.
+ */
+export function seededRandom(seed) {
+    const seedFn = xmur3(seed);
+    return mulberry32(seedFn());
+}
