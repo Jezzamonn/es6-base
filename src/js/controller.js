06ac91438817d8ef54797a02c6be996b666d96b2
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-05-12 07:59:12 -0700

    Fix up whitespace.
    - Removing trailing whitespace
    - Tabs -> Spaces in JS code
    - Spaces -> Tabs in HTML, CSS seeing as those get served.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,34 +1,34 @@
 export default class Controller {
 
-	constructor() {
-		this.animAmt = 0;
-		this.period = 3;
-	}
+    constructor() {
+        this.animAmt = 0;
+        this.period = 3;
+    }
 
-	/**
-	 * Simulate time passing.
-	 *
-	 * @param {number} dt Time since the last frame, in seconds 
-	 */
-	update(dt) {
-		this.animAmt += dt / this.period;
-		this.animAmt %= 1;
-	}
+    /**
+     * Simulate time passing.
+     *
+     * @param {number} dt Time since the last frame, in seconds 
+     */
+    update(dt) {
+        this.animAmt += dt / this.period;
+        this.animAmt %= 1;
+    }
 
-	/**
-	 * Render the current state of the controller.
-	 *
-	 * @param {!CanvasRenderingContext2D} context
-	 */
-	render(context) {
-		context.beginPath();
-		context.fillStyle = 'black';
-		context.moveTo(0, 0);
-		context.arc(0, 0, 100, 0, 2 * Math.PI * this.animAmt);
-		context.fill();
+    /**
+     * Render the current state of the controller.
+     *
+     * @param {!CanvasRenderingContext2D} context
+     */
+    render(context) {
+        context.beginPath();
+        context.fillStyle = 'black';
+        context.moveTo(0, 0);
+        context.arc(0, 0, 100, 0, 2 * Math.PI * this.animAmt);
+        context.fill();
 
-		context.scale(10, 10);
-		context.fillText(this.period * this.animAmt, 0, 0);
-	}
+        context.scale(10, 10);
+        context.fillText(this.period * this.animAmt, 0, 0);
+    }
 
 }
